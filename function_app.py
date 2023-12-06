47cc114c18dcadb23f3519805da45add1120a4cb
Vitaliy Kononenko <konvio@users.noreply.github.com>
2023-12-06 11:05:02 +0200

    Update function app and host configuration

--- function_app.py
+++ function_app.py
@@ -1,6 +1,9 @@
 import azure.functions as func
+
 import datetime
 import json
 import logging
 
-app = func.FunctionApp()
+from app.main import app as fastapi_app
+
+app = func.AsgiFunctionApp(fastapi_app)
